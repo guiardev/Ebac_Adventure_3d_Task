c20bec04e0a46d94c88dc78476f1d4d9b6e7ed94
Guilherme A. R <guiabucarma@gmail.com>
2024-06-12 16:47:43 -0400

    add script GunShootLimit

---
+++ Assets/Scripts/Gun/GunShootLimit.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class GunShootLimit : GunBase{
+
+    private float _currentShoots;
+    private bool _recharging = false;
+
+    public List<UIGunUpdater> uIGunUpdaters;
+
+    public float maxShoot = 5f, timeToRecharge = 1f;
+
+    private void Awake(){
+        GetAllUIs();
+    }
+
+    protected override IEnumerator ShootCoroutine(){
+
+        if (_recharging) yield break; //  yield break returna um quebra
+
+        while (true){
+
+            //verificando os tiros se for menor que o maxShoot
+            if (_currentShoots < maxShoot){
+                Shoot();
+                _currentShoots++;
+                CheckRecharge();
+                UpdateUI();
+                yield return new WaitForSeconds(timeBetweenShoot);
+            }
+        }
+    }
+
+    private void CheckRecharge(){
+
+        if (_currentShoots >= maxShoot){
+            StopShoot();
+            StartRecharge();
+        }
+    }
+
+    private void StartRecharge(){
+        _recharging = true;
+        StartCoroutine(RechargeCoroutine());
+    }
+
+
+    IEnumerator RechargeCoroutine(){
+
+        float time = 0;
+
+        //recarregando balas por um tempo
+        while (time <= timeToRecharge){
+
+            time += Time.deltaTime;
+            Debug.Log("Rechargin: " + time);
+            uIGunUpdaters.ForEach(i => i.UpdateValue(time / timeToRecharge));
+            yield return new WaitForEndOfFrame();
+        }
+
+        _currentShoots = 0;
+        _recharging = false;
+    }
+
+    private void UpdateUI(){
+        uIGunUpdaters.ForEach(i => i.UpdateValue(maxShoot, _currentShoots));
+    }
+
+    private void GetAllUIs(){
+        uIGunUpdaters = GameObject.FindObjectsOfType<UIGunUpdater>().ToList();
+    }
+}
