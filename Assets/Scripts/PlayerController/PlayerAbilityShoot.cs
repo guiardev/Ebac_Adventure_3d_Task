a670883dcf909f1da250b2d9370a722f0f1e8729
Guilherme A. R <guiabucarma@gmail.com>
2024-06-17 14:16:17 -0400

    add weapon switching

--- Assets/Scripts/PlayerController/PlayerAbilityShoot.cs
+++ Assets/Scripts/PlayerController/PlayerAbilityShoot.cs
@@ -7,21 +7,35 @@ public class PlayerAbilityShoot : PlayerAbilityBase{
 
     private GunBase _currentGun;
     public Transform gunPosition;
-    public GunBase gunBase;
+    public GunBase gunBaseShootLimit, gunBaseAngle;
+
+    [Header("Gun Key Setup")]
+    public KeyCode gunShootLimit = KeyCode.Alpha1;
+    public KeyCode gunAngle = KeyCode.Alpha2;
 
     protected override void Init(){
 
         base.Init();
-        CreateGun();
 
         //shootAction.performed += ctx => Shoot();
         inputs.Gameplay.Shoot.performed += cts => StartShoot();  // quando essa botão clicado vai chamar método Shoot();
         inputs.Gameplay.Shoot.canceled += cts => CancelShoot();
     }
 
-    private void CreateGun(){
+    private void Update() {
+        
+        if (Input.GetKeyDown(gunShootLimit)){
+            Debug.Log("gunShootLimit");
+            CreateGun(gunBaseShootLimit);
+        }else if(Input.GetKeyDown(gunAngle)){
+            Debug.Log("gunAngle");
+            CreateGun(gunBaseAngle);
+        }
+    }
 
-        _currentGun = Instantiate(gunBase, gunPosition);
+    private void CreateGun(GunBase gun){
+
+        _currentGun = Instantiate(gun, gunPosition);
 
         _currentGun.transform.localPosition = _currentGun.transform.localEulerAngles = Vector3.zero;
     }
