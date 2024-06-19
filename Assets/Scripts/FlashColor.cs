7ebbe19212b142199ed6dd964deed38e4a1521e8
Guilherme A. R <guiabucarma@gmail.com>
2024-06-19 17:45:27 -0400

    add script FlashColor

---
+++ Assets/Scripts/FlashColor.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class FlashColor : MonoBehaviour{
+
+    public MeshRenderer meshRenderer;
+
+    [Header("Setup")]
+    private Tween _currentTween;
+    private Color _defaultColor;
+    public Color color = Color.red;
+    public float duration = .1f;
+
+
+    private void Start(){
+        _defaultColor = meshRenderer.material.GetColor("_EmissionColor");
+    }
+
+    [NaughtyAttributes.Button]
+    public void Flash(){// _EmissionColor e um variável do Material.
+
+        if (!_currentTween.IsActive()){
+            _currentTween = meshRenderer.material.DOColor(color, "_EmissionColor", duration).SetLoops(2, LoopType.Yoyo); // LoopType e para fazer piscar quando inimigo for atingindo
+        }
+    }
+}
