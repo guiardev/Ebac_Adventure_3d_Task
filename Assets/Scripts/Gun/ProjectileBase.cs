0d552d149feea62cf6326a21ade3143835816ffb
Guilherme A. R <guiabucarma@gmail.com>
2024-07-17 17:44:39 -0400

    Update script ProjectileBase

--- Assets/Scripts/Gun/ProjectileBase.cs
+++ Assets/Scripts/Gun/ProjectileBase.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ProjectileBase : MonoBehaviour{
 
+    public List<string> tagsToHit;
     public float timeToDestroy = 2f, speed = 50f;
     public int damageAmount = 1;
 
@@ -18,11 +19,30 @@ public class ProjectileBase : MonoBehaviour{
 
     private void OnCollisionEnter(Collision col){
 
-        var damageable = col.transform.GetComponent<IDamageable>();
 
-        if (damageable != null){
-            damageable.Damage(damageAmount);
-            Destroy(gameObject);
+        foreach (var t in tagsToHit){
+
+            if (col.transform.tag == t){
+
+                var damageable = col.transform.GetComponent<IDamageable>();
+
+                if (damageable != null){
+
+                    //fazendo personagem que levar tiro sentir impacto do tiro na direção oposta
+                    //0,0,35
+                    Vector3 dir = col.transform.position - transform.position;
+                    //0,0,1
+                    dir = -dir.normalized;
+
+                    dir.y = 0; // zerando posição y para personagem atingindo nao subir
+
+                    damageable.Damage(damageAmount, dir);
+                    Destroy(gameObject);
+                }
+
+                break;
+            }
         }
+
     }
 }
