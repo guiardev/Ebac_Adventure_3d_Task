9553709142dcce9fe593c300d18e8bad8b7b03ba
Guilherme A. R <guiabucarma@gmail.com>
2024-07-17 17:44:02 -0400

    Update script FlashColor

--- Assets/Scripts/FlashColor.cs
+++ Assets/Scripts/FlashColor.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class FlashColor : MonoBehaviour{
 
     public MeshRenderer meshRenderer;
+    public SkinnedMeshRenderer skinnedMeshRenderer;
+
 
     [Header("Setup")]
     private Tween _currentTween;
