d70b9b431f4ee8612e1d13ebd96cb735ec5fdc07
Guilherme A. R <guiabucarma@gmail.com>
2024-08-02 14:43:32 -0400

    Add script UIFillUpdater

---
+++ Assets/Scripts/Gun/UIFillUpdater.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class UIFillUpdater : MonoBehaviour{
+
+
+    private Tween _currTween;
+    public Image uiImage;
+
+    [Header("Animation")]
+    public Ease ease = Ease.OutBack;
+    public float duration = .1f;
+
+    private void OnValidate(){
+
+        if (uiImage == null){
+            uiImage = GetComponent<Image>();
+        }
+    }
+
+    public void UpdateValue(float f){
+        uiImage.fillAmount = f;
+    }
+
+    public void UpdateValue(float max, float current){
+
+        if (_currTween != null) _currTween.Kill();
+
+        //10/100 = .1
+        //uiImage.fillAmount = 1 - (current / max);
+        _currTween = uiImage.DOFillAmount(1 - (current / max), duration).SetEase(ease); // 1 - (current / max) <-- fazendo ele inverter carregamento da image
+
+    }
+}
