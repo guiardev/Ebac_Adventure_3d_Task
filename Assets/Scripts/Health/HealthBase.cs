44a67cc69cf0f6e9a137e3a2432b23813f7a5da1
Guilherme A. R <guiabucarma@gmail.com>
2024-08-16 17:25:13 -0400

    Update script HealthBase

--- Assets/Scripts/Health/HealthBase.cs
+++ Assets/Scripts/Health/HealthBase.cs
@@ -21,6 +21,7 @@ public class HealthBase : MonoBehaviour, IDamageable{
 
     public void ResetLife(){
         _currentLife = startLife;
+        UpdateUI();
     }
 
     protected virtual void Kill(){
