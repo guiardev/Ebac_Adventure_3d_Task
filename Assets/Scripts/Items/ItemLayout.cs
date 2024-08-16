8615f69d437289290ab43b07a98718bbc3e0af57
Guilherme A. R <guiabucarma@gmail.com>
2024-08-16 17:23:17 -0400

    add script ItemLayout in Items

---
+++ Assets/Scripts/Items/ItemLayout.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+namespace Itens{
+
+    public class ItemLayout : MonoBehaviour{
+
+        private ItemSetup _currSetup;
+
+        public Image uiIcon;
+        public TMP_Text uiValue;
+
+        public void Load(ItemSetup setup){
+            _currSetup = setup;
+            UpdateUI();
+        }
+
+        private void UpdateUI(){
+            uiIcon.sprite = _currSetup.icon;
+        }
+
+        private void Update() {
+            uiValue.text = _currSetup.soInt.value.ToString();
+        }
+
+    }
+}
