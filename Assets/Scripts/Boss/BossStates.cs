9cf0c601766430a94a951a6e4b882b845bd0549c
Guilherme A. R <guiabucarma@hotmail.com>
2025-02-17 16:30:26 -0400

    Update BossStates
    add debug logs

--- Assets/Scripts/Boss/BossStates.cs
+++ Assets/Scripts/Boss/BossStates.cs
@@ -7,15 +7,15 @@ namespace Boss{
 
     public class BossStateBase : StateBase{
 
-        protected BossBase boss; // <-- Index was outside the bounds of the array.
+        protected BossBase boss;
 
         public override void OnStateEnter(params object[] objs){
 
             Debug.Log("boss " + boss);
             Debug.Log("objs[] " + objs);
-            Debug.Log("(BossBase)objs[0] " + objs[0]); // <-- Index was outside the bounds of the array.
+            Debug.Log("(BossBase)objs[0] " + objs[0]);
             base.OnStateEnter(objs);
-            boss = (BossBase)objs[0]; // <-- Index was outside the bounds of the array.
+            boss = (BossBase)objs[0];
         }
     }
 
@@ -33,19 +33,21 @@ namespace Boss{
         
         public override void OnStateEnter(params object[] objs){
 
-            Debug.Log("OnStateEnter ");
+            Debug.Log("BossStatesWalk objs" + objs);
             base.OnStateEnter(objs);
             boss.GoToRandomPoint(OnArrive);
         }
 
         private void OnArrive(){  // mudando stato boss quando ele chegar no destino
             boss.SwitchState(BossAction.ATTACK);
+            Debug.Log("method OnArrive");
+            
         }
 
         public override void OnStateExit(){
-            //Debug.Log("Exit Attack");
             base.OnStateExit();
             boss.StopAllCoroutines();
+            Debug.Log("OnStateExit Exit Attack");
         }
     }
 
